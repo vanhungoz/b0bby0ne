d6c132ae0dfd73af4da8063f4a8bb8e84983c284
Duy Pham <b0bby0ne@Duys-MacBook-Pro.local>
2019-03-06 22:22:49 +0700

    a

--- trash.js
+++ trash.js
@@ -9,21 +9,21 @@ var io = require("socket.io")(server);
 
 const mysql = require("mysql");
 
-var sql = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "NodeJs"
-});
+// var sql = mysql.createConnection({
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "",
+//   database: "NodeJs"
+// });
 
-sql.connect(err => {
-  if (err) {
-    console.log("error", err);
-    return;
-  }
+// sql.connect(err => {
+//   if (err) {
+//     console.log("error", err);
+//     return;
+//   }
 
-  console.log("SQL connect Successfully");
-});
+//   console.log("SQL connect Successfully");
+// });
 
 server.listen(3000);
 
